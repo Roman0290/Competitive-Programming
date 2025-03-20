fa7eb027c3c254d3d627b72c0ab00e62f6482b49
Roman Kebede <149621463+Roman0290@users.noreply.github.com>
2025-03-20 15:41:40 +0300

    Time: 49 ms (64.09%), Space: 18.1 MB (58.05%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -1,18 +1,14 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         seen = set()
-        count = 0
+        count = 1
 
         for char in s:
             if char in seen:
-                
                 count += 1
                 seen.clear()
             seen.add(char)
-
-   
-        if seen:
-            count += 1
+        
 
         return count
 
