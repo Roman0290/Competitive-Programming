9d9bc12e36e5c241186f20f9fd284b6eb2c275eb
Roman Kebede <149621463+Roman0290@users.noreply.github.com>
2025-04-04 11:31:46 +0300

    Time: 104 ms (48.88%), Space: 28.5 MB (67.45%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,33 +1,21 @@
+from typing import List
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        #brute force techniques
-        #time complexity o(n*2)
-        # res=0
-        # for left in range(len(height)):
-        #     for right in range(left+1,len(height)):
-        #         area=(right-1)* min(height[left],height[right])
-        #         res=max(res,area)
-        # return res
+        # Optimized two-pointer approach
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        res = 0
+        left = 0
+        right = len(height) - 1
 
+        while left < right:
+            area = (right - left) * min(height[left], height[right])
+            res = max(res, area)
 
-    #time complexity= o(N
-    #space complexity=o(1) because it uses constant space
-        res=0
-        left=0
-        right=len(height)-1
-        
-        while left<right:
-            area= (right-left)* min(height[left],height[right])
-            res=max(res,area)
-            
-            if height[left]<height[right]:
-                left+=1
-            # elif height[left]>height[right]:
-            #     right-=1
-            else :
-                right-=1
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
         return res
-
-
-
-        
