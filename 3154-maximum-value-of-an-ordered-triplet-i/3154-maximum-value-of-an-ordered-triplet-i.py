cf9e270510eb274cd49a62f05375a0014ec98420
Roman Kebede <149621463+Roman0290@users.noreply.github.com>
2025-04-16 10:03:31 +0300

    Time: 3 ms (75.03%), Space: 18 MB (14.55%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -1,10 +1,21 @@
 class Solution(object):
     def maximumTripletValue(self, nums):
+        n = len(nums)
+        left_max = [0] * n
+        right_max = [0] * n
+        
+        left_max[0] = nums[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i-1], nums[i])
+        
+        right_max[-1] = nums[-1]
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(right_max[i+1], nums[i])
+        
         maxTriplet = 0
-        for i in range(len(nums)):
-            for k in range(len(nums) - 1, i, -1):
-                j = i + 1
-                while j < k:
-                    maxTriplet = max(maxTriplet, (nums[i] - nums[j]) * nums[k])
-                    j += 1
+        for j in range(1, n-1):
+            current_val = (left_max[j-1] - nums[j]) * right_max[j+1]
+            if current_val > maxTriplet:
+                maxTriplet = current_val
+        
         return max(0, maxTriplet)
